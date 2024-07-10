9cc22995ab2644984c2e7f9bdb67ead748dd0494
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-10 20:02:25 +0000

    added google gemini api

--- app/controllers/omdb.php
+++ app/controllers/omdb.php
@@ -10,6 +10,35 @@ class Omdb extends Controller {
     
     echo "<pre>";
     print_r ($movie);
+
+    $url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=".$_ENV['GEMINI'];
+
+    $data = array(
+      "contents" => array(
+        array(
+          "role" => "user",
+          "parts" => array(
+            array(
+              "text" => 'Write the numbers 1 to 10 in reverse order'
+            )
+          )
+        )
+      )
+    );
+
+    $json_data = json_encode($data);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $response = curl_exec($ch);
+    if(curl_errno($ch)) {
+      echo 'Curl error: ' . curl_error($ch);
+    }
+
+    echo "<pre>";
+    echo $response;  
     die;
 
   }
