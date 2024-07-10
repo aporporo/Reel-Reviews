47d96bb1ce15459d35f3868ee0ca9f5fbdbc0a3a
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-10 19:36:47 +0000

    added omdb api access

---
+++ app/views/omdb/index.php
@@ -0,0 +1,23 @@
+<?php require_once 'app/views/templates/header.php' ?>
+
+
+<div class="container">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                omdb
+
+
+            </div>
+        </div>
+    </div>
+
+
+
+    
+
+    <br>
+
+
+
+    <?php require_once 'app/views/templates/footer.php' ?>
