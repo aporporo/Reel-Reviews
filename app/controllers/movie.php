6ac40c3f7d0cfbb03fef360c0ad5616242d4783b
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-11 21:52:06 +0000

    added functionality to use search bar to look for movies

--- app/controllers/movie.php
+++ app/controllers/movie.php
@@ -7,12 +7,17 @@ class Movie extends Controller {
 
     $movie_title = "the matrix";
     $movie = $api->getMovie($movie_title);
-    // $review = $api->getReview($movie_title);
+    $review = $api->getReview($movie_title);
     $data = [
       'movie' => $movie,
       'review' => $review
     ];
     $this->view('movie/index' , $data);
+  }
+
+  public function find($movie_title) {
+    $api = $this->model('Api');
+    $this->view('movie/results/', $movie_title);
   }
 
   public function search() {
@@ -23,7 +28,8 @@ class Movie extends Controller {
 
     $api = $this->model('Api');
 
-    $movie_title = "the matrix";
+    $movie_title = $_REQUEST['search'];
+    echo $movie_title;
     $movie = $api->getMovie($movie_title);
     $review = $api->getReview($movie_title);
     // $review2 = $api->getReview($movie_title);
@@ -39,6 +45,28 @@ class Movie extends Controller {
     ];
 
     
+
+    $this->view('movie/results', $data);
+    header('location: /movie/results');
+  }
+
+  public function searchByTitle($movie_title) {
+    
+
+    $api = $this->model('Api');
+
+    $movie_title = "the matrix";
+    $movie = $api->getMovie($movie_title);
+    $review = $api->getReview($movie_title);
+    
+
+    $data = [
+      'movie' => $movie,
+      'review' => $review,
+      
+    ];
+
+
 
     $this->view('movie/results', $data);
     header('location: /movie/results');
