752120e14b67143bbbb22a81c60f2aaafb252f44
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-13 18:24:17 +0000

    deleted unnecessary info from home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -57,27 +57,7 @@
 
   </div>
     
-    <div class="page-header" id="banner">
-        
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 style="color: white">Welcome, <?php echo $_SESSION['username'] ?></h1>
-                <p class="lead" style="color: white"> <?= date("F jS, Y"); ?></p>
-
-                
-            </div>
-        </div>
-    </div>
-
     
-
-    <div class="row">
-        <div class="col-lg-12">
-            <p> <a href="/logout">Click here to logout</a></p>
-        </div>
-    </div>
-
-    <br>
 
     
 
