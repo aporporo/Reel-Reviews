feafbc9e661c4177b2258b288a20cefee6bf0382
aporporo <117672857+aporporo@users.noreply.github.com>
2024-07-14 14:59:40 +0000

    added links to the movie posters

--- app/views/movie/index.php
+++ app/views/movie/index.php
@@ -1,10 +1,31 @@
 <?php require_once 'app/views/templates/header.php' ?>
 
+<style>
+    .col-md-7 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
 
+    }
+
+    .col-md-5 {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
+    }
+
+    .featurette {
+        padding: 1rem 0;
+    }
+</style>
 <div class="container">
     <div class="row featurette">
       <div class="col-md-7">
-        <h2 class="featurette-heading fw-normal lh-1">Check out some movies. <span class="text-body-secondary">Rate and Review!</span></h2>
+        <h2 class="featurette-heading fw-normal lh-1">Check out some movies. <span class="text-body-secondary">Rate and review!</span></h2>
         <p class="lead">Below are just some of the movies you can search and learn more about.</p>
       </div>
       <div class="col-md-5">
@@ -15,20 +36,21 @@
     <hr class="featurette-divider">
 
     <h1 class="display-5">Just some of our movies</h1>
-
+    
     <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <div class="card h-100">
-          <img src="<?php echo $data['movie1']['Poster'] ?>" class="card-img-top">
-          <div class="card-body">
-            <h5 class="card-title"><?php echo $data['movie1']['Title'] ?></h5>
-            <p class="card-text"><?php echo $data['movie1']['Plot'] ?></p>
-          </div>
+        
+        <div class="col">
+            <div class="card h-100">
+                <a href="/movie/search"><img src="<?php echo $data['movie1']['Poster'] ?>" class="card-img-top"></a>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $data['movie1']['Title'] ?></h5>
+                    <p class="card-text"><?php echo $data['movie1']['Plot'] ?></p>
+                </div>
+            </div>
         </div>
-      </div>
       <div class="col">
         <div class="card h-100">
-          <img src="<?php echo $data['movie2']['Poster'] ?>" class="card-img-top">
+            <a href="/movie/search"><img src="<?php echo $data['movie2']['Poster'] ?>" class="card-img-top"></a>
           <div class="card-body">
             <h5 class="card-title"><?php echo $data['movie2']['Title'] ?></h5>
             <p class="card-text"><?php echo $data['movie2']['Plot'] ?></p>
@@ -37,7 +59,7 @@
       </div>
       <div class="col">
         <div class="card h-100">
-          <img src="<?php echo $data['movie3']['Poster'] ?>" class="card-img-top">
+            <a href="/movie/search"><img src="<?php echo $data['movie3']['Poster'] ?>" class="card-img-top"></a>
           <div class="card-body">
             <h5 class="card-title"><?php echo $data['movie3']['Title'] ?></h5>
             <p class="card-text"><?php echo $data['movie3']['Plot'] ?></p>
